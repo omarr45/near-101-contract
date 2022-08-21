2b664ca0dd197ae2c4aed631b8bbfd30629b3564
Omar AbdulRahman <58887202+omarr45@users.noreply.github.com>
2022-08-21 18:13:40 +0200

    Added better comments

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,33 +1,51 @@
 import { ContractPromiseBatch, context } from 'near-sdk-as';
 import { Quote, likedQuotes, listedQuotes } from './model';
 
+// add a new quote to the contract
 export function addQuote(quote: Quote): string {
+  // check if the quote already exists
   let storedQuote = listedQuotes.get(quote.id);
   if (storedQuote !== null) {
     throw new Error(`a quote with ${quote.id} already exists`);
   }
+
+  // add the quote to the contract
   listedQuotes.set(quote.id, Quote.fromPayload(quote));
+
+  // confirm the quote was added
   return `quote ${quote.text} with id ${quote.id} was added successfully`;
 }
 
+// Get a quote by its ID
 export function getQuote(id: string): Quote | null {
   return listedQuotes.get(id);
 }
 
+// Get all quotes
 export function getQuotes(): Quote[] {
   return listedQuotes.values();
 }
 
+// Like a quote
 export function likeQuote(quoteId: string): string {
+  // check if the quote exists
   const quote = getQuote(quoteId);
   if (quote == null) {
     throw new Error('quote not found');
   }
+
+  // check if attached deposit is enough to like the quote
   if (quote.price.toString() != context.attachedDeposit.toString()) {
     throw new Error("attached deposit should equal to the quote's price");
   }
+
+  // Send the deposit to the quote owner
   ContractPromiseBatch.create(quote.owner).transfer(context.attachedDeposit);
+
+  // increase the quote likes counter
   quote.incrementLikes();
+
+  // update the quote in the contract (to update likes counter)
   listedQuotes.set(quote.id, quote);
 
   // Get the old list of liked quotes
@@ -47,6 +65,7 @@ export function likeQuote(quoteId: string): string {
   return `${context.sender} liked "${quote.text}" with id ${quote.id} successfully`;
 }
 
+// Get all liked quotes by a user
 export function getUserQuotes(): Quote[] {
   let quotes: Quote[] = [];
 
--- assembly/model.ts
+++ assembly/model.ts
@@ -2,12 +2,12 @@ import { PersistentUnorderedMap, context, u128 } from 'near-sdk-as';
 
 @nearBindgen
 export class Quote {
-  id: string;
-  text: string;
-  character: string;
-  price: u128;
-  owner: string;
-  likes: u32;
+  id: string; // unique ID for the quote
+  text: string; // the text of the quote
+  character: string; // the person who said the quote
+  price: u128; // the price to like the quote
+  owner: string; // the owner of the quote
+  likes: u32; // the number of likes the quote has
 
   public static fromPayload(payload: Quote): Quote {
     const quote = new Quote();
@@ -19,13 +19,16 @@ export class Quote {
     return quote;
   }
 
+  // increase likes counter
   public incrementLikes(): void {
     this.likes = this.likes + 1;
   }
 }
 
+// container for all listed quotes on the contract {quote ID : Quote}
 export const listedQuotes = new PersistentUnorderedMap<string, Quote>('lq');
 
+// container for all liked quotes on the contract {user ID : Array of Quotes}
 export const likedQuotes = new PersistentUnorderedMap<string, Array<string>>(
   'ulq'
 );
